b7b16f9202b8518ec1a80df1594914a79ad335b3
Daniel Mauricio Gutierrez Vanegas <dm.gutierrez@globant.com>
2022-07-21 15:16:27 -0500

    Check functionality

--- controllers/categories/index.js
+++ controllers/categories/index.js
@@ -2,82 +2,76 @@ const { Router, response } = require('express');
 const router = Router();
 const { check } = require('express-validator');
 
-const { add, update, getAll } = require('../../services/categories/category_services');
+const { add, update, getAll } = require('../../services/category');
 
 const { validateFields } = require('../../core/middlewares/validate_fields');
 
 const createCategory = (req, res = response) => {
-    try {
-        const { name, description } = req.body;
-        const categoryAdded = add({ name, description });
-        res.status(201).json({
-            ok: true,
-            uid: categoryAdded.id,
-            name: categoryAdded.name
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Server error'
-        });
-    }
+  try {
+    const { name, description } = req.body;
+    const categoryAdded = add({ name, description });
+    res.status(201).json({
+      ok: true,
+      uid: categoryAdded.id,
+      name: categoryAdded.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Server error',
+    });
+  }
 };
 
 const updateCategory = (req, res = response) => {
-    try {
-        const { id } = req.params;
-        const { name, description } = req.body;
-        const categoryUpdated = update({ id, name, description });
-        res.status(200).json({
-            ok: true,
-            uid: categoryUpdated.id,
-            name: categoryUpdated.name
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Server error'
-        });
-    }
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const categoryUpdated = update({ id, name, description });
+    res.status(200).json({
+      ok: true,
+      uid: categoryUpdated.id,
+      name: categoryUpdated.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Server error',
+    });
+  }
 };
 
 const getAllCategory = (req, res = response) => {
-    try {
-        const categories = getAll();
-        res.status(200).json({
-            ok: true,
-            categories
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Server error'
-        });
-    }
+  try {
+    const categories = getAll();
+    res.status(200).json({
+      ok: true,
+      categories,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Server error',
+    });
+  }
 };
 
 router.post(
-    '/new', 
-    [
-        check('name', 'Title is required').trim().not().isEmpty(),
-        check('description', 'Description is required').trim().not().isEmpty(),
-        validateFields
-    ],  
-    createCategory
+  '/new',
+  [
+    check('name', 'Title is required').trim().not().isEmpty(),
+    check('description', 'Description is required').trim().not().isEmpty(),
+    validateFields,
+  ],
+  createCategory
 );
 
 router.patch(
-    '/update/:id', 
-    [
-        check('name', 'Name is required').trim().not().isEmpty(),
-        validateFields
-    ],  
-    updateCategory
+  '/update/:id',
+  [check('name', 'Name is required').trim().not().isEmpty(), validateFields],
+  updateCategory
 );
 
-router.get(
-    '/', 
-    getAllCategory
-);
+router.get('/', getAllCategory);
 
 module.exports = router;
--- controllers/recipes/index.js
+++ controllers/recipes/index.js
@@ -2,113 +2,111 @@ const { Router, response } = require('express');
 const router = Router();
 const { check } = require('express-validator');
 
-const { add, addIngredientsToRecipe, 
-        addImage, addCategoriesToRecipe } = require('../../services/recipes/recipe_services');
+const {
+  add,
+  addIngredientsToRecipe,
+  addImage,
+  addCategoriesToRecipe,
+} = require('../../services/recipe');
 
 const { validateFields } = require('../../core/middlewares/validate_fields');
 const { validateFile } = require('../../core/middlewares/validate_file');
 
 const createRecipe = (req, res = response) => {
-    try {
-        add(req.body);
-        res.status(201).json({
-            ok: true,
-            msg: 'Recipe added'
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Server error'
-        });
-    }
+  try {
+    add(req.body);
+    res.status(201).json({
+      ok: true,
+      msg: 'Recipe added',
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Server error',
+    });
+  }
 };
 
 const addIngredients = (req, res = response) => {
-    try {
-        const { id } = req.params;
-        const { ingredients } = req.body;
-        addIngredientsToRecipe(id, ingredients);
-        res.status(201).json({
-            ok: true,
-            msg: 'Ingredients added'
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Server error'
-        });
-    }
+  try {
+    const { id } = req.params;
+    const { ingredients } = req.body;
+    addIngredientsToRecipe(id, ingredients);
+    res.status(201).json({
+      ok: true,
+      msg: 'Ingredients added',
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Server error',
+    });
+  }
 };
 
 const addCategories = (req, res = response) => {
-    try {
-        const { id } = req.params;
-        const { categories } = req.body;
-        addCategoriesToRecipe(id, categories);
-        res.status(201).json({
-            ok: true,
-            msg: 'Categories added'
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Server error'
-        });
-    }
+  try {
+    const { id } = req.params;
+    const { categories } = req.body;
+    addCategoriesToRecipe(id, categories);
+    res.status(201).json({
+      ok: true,
+      msg: 'Categories added',
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Server error',
+    });
+  }
 };
 
 const addImages = (req, res = response) => {
-    try {
-        const { id } = req.params;
-        const { img } = req.files;
-        addImage(id, img);
-        res.status(201).json({
-            ok: true,
-            msg: 'Image added'
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: error
-        });
-    }
+  try {
+    const { id } = req.params;
+    const { img } = req.files;
+    addImage(id, img);
+    res.status(201).json({
+      ok: true,
+      msg: 'Image added',
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: error,
+    });
+  }
 };
 
 router.post(
-    '/new', 
-    [
-        check('title', 'Title is required').trim().not().isEmpty(),
-        check('description', 'Description is required').trim().not().isEmpty(),
-        check('preparation', 'Preparation is required').trim().not().isEmpty(),
-        validateFields,
-    ],  
-    createRecipe
+  '/new',
+  [
+    check('title', 'Title is required').trim().not().isEmpty(),
+    check('description', 'Description is required').trim().not().isEmpty(),
+    check('preparation', 'Preparation is required').trim().not().isEmpty(),
+    validateFields,
+  ],
+  createRecipe
 );
 
 router.post(
-    '/:id/add/ingredients', 
-    [
-        check('ingredients', 'Ingregients is required').not().isEmpty(),
-        validateFields,
-    ],  
-    addIngredients
+  '/:id/add/ingredients',
+  [
+    check('ingredients', 'Ingregients is required').not().isEmpty(),
+    validateFields,
+  ],
+  addIngredients
 );
 
 router.post(
-    '/:id/add/categories', 
-    [
-        check('categories', 'Categories is required').not().isEmpty(),
-        validateFields,
-    ],  
-    addCategories
+  '/:id/add/categories',
+  [
+    check('categories', 'Categories is required').not().isEmpty(),
+    validateFields,
+  ],
+  addCategories
 );
 
-
-router.post(
-    '/:id/add/images', 
-    validateFile,
-    addImages
-);
-
+router.post('/:id/add/images', validateFile, addImages);
 
 module.exports = router;
